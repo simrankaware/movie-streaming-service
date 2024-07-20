811f868cbdf693324f02e2d8d97e20a7454a97f6
Simran Kaware <sk4122@ic.ac.uk>
2024-07-20 14:19:50 +0100

    Add jumbotron component

--- movie-streaming-service/src/app.js
+++ movie-streaming-service/src/app.js
@@ -1,6 +1,9 @@
+import React from 'react';
+import { JumbotronContainer } from './containers/jumbotron';
+
 export default function App() {
   return (
-    <p>Hello</p>
+    <JumbotronContainer/>
   );
 }
 
---
+++ movie-streaming-service/src/components/jumbotron/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import "./styles/jumbotron.css"
+export default function Jumbotron({ children, direction = 'row', ...restProps }) {
+  return (
+    <div className="item" {...restProps}>
+      <div direction={direction}>
+      <div className="inner" data-direction={direction} {...restProps}>
+        {children}
+      </div>
+      </div>
+    </div>
+  );
+}
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+  return <div className="container" {...restProps}>{children}</div>;
+};
+
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+  return <div className="pane" {...restProps}>{children}</div>;
+};
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+  return <h1 className="title" {...restProps}>{children}</h1>;
+};
+
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+  return <h2 className="subtitle" {...restProps}>{children}</h2>;
+};
+
+Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+  return <img className="img" {...restProps} />;
+};
